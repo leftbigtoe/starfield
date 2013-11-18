9da6ce799bb156ff0ccb1e2d013deb8ceb3ef3e8
leftbigtoe <left_leg@rocketmail.com>
2013-11-18 22:45:13 +0100

    initial upload

---
+++ classroomTrials.py
@@ -0,0 +1,172 @@
+﻿
+import helperFunctions
+import viz
+import vizact
+import viztask
+
+class passageActionClass(viz.ActionClass):
+	
+	def begin(self, object):
+		"""
+		constructor, gets parameters from self.data object (see vizard tutorial for writing own
+		action classes to understand those weird ways)
+		"""
+		# get data
+		self.data = self._actiondata_.data
+		self.turningAngle 	= self.data[0]
+		self.vLinear 		= self.data[1]	
+		self.tLegs 			= self.data[2]
+		self.tAccLin	 	= self.data[3]
+		
+		# set internal parameters
+		self.t 				= 0
+		self.direction 		= -1 if self.turningAngle < 0 else 1
+		self.accHeading 	= 0
+		self.currentHeading = object.getAxisAngle(viz.ABS_GLOBAL)[3]
+		self.aTurn			= 15  	# fixed turn aceleration, be cautious with higher values (nausea)
+		self.vLinCurrent	= 0.0
+
+		# calculate linear acceleration time needed
+		self.aLinear = self.vLinear / self.tAccLin
+		
+		# time after half the turn is done and deccelerating can begin
+		self.tAccCirc = (abs(self.turningAngle)/self.aTurn)**0.5		
+		
+		
+	def update(self, elapsed, object):
+		"""
+		update function, executed every frame
+		"""
+		### updating parameters
+		# Update time
+		tLast = self.t
+		self.t += elapsed
+
+		
+		### control turn movement
+		deltaSCirc = 0
+		# get turn time (=0 at beginning of turn)
+		timeCirc = self.t - (self.tLegs[0] + self.tAccLin)
+		## accelerating part of the turn
+		if 0 < timeCirc and timeCirc <= self.tAccCirc:			
+			# calculate Heading after certain time of accelerating the turn (save in buffer)
+			self.accHeading = 0.5 * self.aTurn * timeCirc**2
+			# if still accelerating, take value as current heading
+			self.currentHeading = self.accHeading
+			
+		## decelerating part of the turn
+		if self.tAccCirc < timeCirc and timeCirc <= 2*self.tAccCirc:
+			# displacement via constant speed after acceleration - 'deceleration movement'
+			self.currentHeading = ((self.aTurn * self.tAccCirc) * (timeCirc - self.tAccCirc)) - (
+								  0.5 * self.aTurn * (timeCirc - self.tAccCirc)**2) + self.accHeading
+		
+		
+		### control linear movement
+		# normally constant linear movement, during acceleration phases overwritten
+		deltaS = elapsed * self.vLinear
+		
+		## accelerate linear movement
+		if self.t <= self.tAccLin:
+			# calculate linear movement since last frame (overwrite linear deltaS)
+			deltaS =  0.5 * self.aLinear * self.t**2 - 0.5 * self.aLinear * tLast**2
+
+		## decelerate linear movement
+		timeLin = self.t - (self.tLegs[0] + self.tLegs[1] + self.tAccLin + 2 * self.tAccCirc)
+		if 0 < timeLin:
+			# deceleration deltaS is linear movement - deceleration during last frame
+			deltaS = (self.vLinear * elapsed) - (  0.5 * self.aLinear *  timeLin            ** 2 
+											     - 0.5 * self.aLinear * (timeLin - elapsed) ** 2)
+			
+			
+		### end movement object eventually
+		if deltaS < 0:
+			self.end(object)
+	
+		### updating position of the virtual avatar
+		# update avatar according to linear displacement
+		object.setPosition([0,0,deltaS], viz.ABS_LOCAL)
+		
+		# update avatar orientation
+		object.setAxisAngle([0,1,0,self.currentHeading * self.direction],viz.ABS_GLOBAL)
+
+		# save current heading as last heading
+		self.lastHeading  = self.currentHeading
+		
+		
+	def end(self, object):	
+		# finally, the end		
+		viz.ActionClass.end(self,object)
+	
+	
+def passageAction(turningAngle, vLinear = 8, tLegs = [3,4], tAccLin = 1):
+	"""
+	create a passage action independent of the treadmill. Offers linearly
+	accelerated linear and turn move.
+	turningAngle: 	total angle of the turn (negative for left turns)
+	vLinear:      	max linear speed
+	tLegs:		  	purely linear (straight) movement without turn 
+	tAccLin:		time used for accelerating to vLinear (adds to tLegs)
+	"""
+	bla = viz.ActionData()
+	bla.data =[turningAngle, vLinear, tLegs, tAccLin]
+	bla.actionclass = passageActionClass
+	return bla	
+
+
+def strippedTrialTask(avatar, turningAngle, vLinear, tLegs, idxTrial, output = {}):
+	"""
+	master task that invokes all other tasks as subtasks to ensure right order
+	"""
+	# add text for instructions
+	screenText = viz.addText('trial no. '+str(idxTrial),viz.SCREEN)
+	screenText.setBackdrop(viz.BACKDROP_RIGHT_BOTTOM)
+	screenText.setBackdropColor(viz.GRAY)
+	screenText.setPosition(0.05, 0.5)
+	
+	# wait for key press and execute trial
+	yield viztask.waitKeyDown(' ')
+	screenText.message('')
+	passage = passageAction(turningAngle, vLinear, tLegs)
+	avatar.addAction(passage)
+	yield viztask.waitActionEnd(avatar, passage)
+	
+	# get homing vectors
+	yield helperFunctions.saveHomingVectors(avatar, output)
+	print output
+	
+	# post trial instructions & position reset
+	yield viztask.waitTime(0.2)
+	screenText.message('please select your\nanswer for trial '+str(idxTrial))
+	yield viztask.waitKeyDown(' ')
+	screenText.remove()
+	yield helperFunctions.resetTask(avatar)
+
+
+def multipleTrialTask(avatar, angles):
+	"""
+	function to call multiple trials (needs to be a task to ensure proper time course)
+	"""
+	counter = 1
+	for angle in trialDict['turningAngles']:
+		output = {}
+		yield viztask.waitTask(strippedTrialTask(avatar, angle, trialDict['vLinear'], 
+												 trialDict['tLegs'], counter))
+		counter+=1
+	# add text for instructions
+	screenText = viz.addText('please open and fill out\nthe questionnaires now',viz.SCREEN)
+	screenText.setBackdrop(viz.BACKDROP_RIGHT_BOTTOM)
+	screenText.setBackdropColor(viz.GRAY)
+	screenText.setPosition(0.05, 0.5)
+
+
+## set up world 
+avatar = helperFunctions.createWorld()
+viz.go(viz.FULLSCREEN)
+
+## set parameters here
+outputDict = {}
+trialDict = {'turningAngles':[-60, 90, -90, 60], 'vLinear':10, 'tLegs':[2,3]}
+
+# starts execution of the script by scheduling the multipleTrialTask		
+viztask.schedule(multipleTrialTask(avatar, trialDict))
+
---
+++ helperFunctions.py
@@ -0,0 +1,293 @@
+﻿import viz
+import viztask
+import vizshape
+import math
+import vizact
+import csv
+import time
+import serial
+import random
+import pickle
+
+
+
+#### set up #####
+
+def createWorld(HMDsensor = None, startingPoint = [0,0,0], fog = True):
+	"""
+	sets up an optical flow environment using the build in precipation 
+	particle system a sphere (to lower visibility range to avoid visual 
+	artifacts) and the mainView are linked to an (invisible) avatar
+	TESTED
+	"""
+
+	# set up view
+	view = viz.MainView
+	 
+	# sphere restrict visibility range (otherwise vision artifacts)
+	bubble = vizshape.addSphere(radius = 1000, 
+			 slices=20, stacks=20, cullFace = False)
+	bubble.color(viz.BLACK)
+
+	# wheelbarrow (to attach view & bubble to)
+	avatar = viz.add('wheelbarrow.ive')
+	avatar.setPosition(startingPoint)
+	avatar.visible(viz.OFF) # uncomment for debugging
+
+	# link viewpoint and object (uncomment for debugging)
+	viz.link(avatar, viz.MainView)
+	# link bubble to object
+	viz.link(avatar, bubble)
+	# eventually, link polhemus to mainview
+	if not HMDsensor == None:
+		lnk  = viz.link(avatar, viz.MainView)
+		lnk.preMultLinkable(HMDsensor)
+	
+	# add starfield as action to action pool 1
+	avatar.addAction(addStarfield(nStars = 12000, starSize = 0.17, colDimXYZ = [6, 30, 6],
+							      nColsXYZ = [21, 1, 21], fog = fog), 1)
+	
+	return avatar
+
+
+
+##### task control ######
+
+def resetTask(avatar, resetStarfield = True, tm = None):
+	"""
+	resets the position of the avatar after the trial
+	TESTED
+	"""
+	# create black quad for fading
+	blackScreen = viz.addTexQuad(parent=viz.SCREEN,scale=[100.0]*3,color=viz.BLACK)
+	blackScreen.alpha(0.0)
+	
+	# fade out
+	fadeOut = vizact.fadeTo(1.0,time=2.0)
+	blackScreen.addAction(fadeOut)
+	yield viztask.waitActionEnd(blackScreen, fadeOut)
+
+	# remove old starfield
+	if resetStarfield:
+		avatar.clearActions(pool = 1)
+	
+	if not(tm == None):
+		tm.__del__()
+		
+	# reset position & heading
+	yield avatar.setPosition([0,0,0])
+	yield avatar.setAxisAngle([0,1,0,0])
+	
+	# create new starfield
+	if resetStarfield:
+		avatar.addAction(addStarfield(), pool = 1)
+	
+	# fade in
+	fadeIn = vizact.fadeTo(0.0,time=2.0)
+	blackScreen.addAction(fadeIn)
+	yield viztask.waitActionEnd(blackScreen, fadeIn)
+	blackScreen.remove()
+
+
+
+#### homing vector calcultion and saving stuff ####
+
+def transformReferenceFrame(heading360, alpha360):
+	""" 
+	given the heading and a certain angle alpha it expresses alpha one frame
+	(egocentric or allocentric) it expresses alpha in the other frame
+	USE 360 DEGREE SYSTEM ANGLES ONLY!!!
+	"""	
+	# substract smaller angle from bigger angle
+	if heading360 > alpha360:
+		transformed = heading360 - alpha360
+	else:
+		transformed = alpha360 - heading360
+	
+	# magical bugfix, not exactly know why
+	# fixes sign error for left turns
+	transformed  *= -1 if heading360  > 180 else 1	
+	
+	# return converted to 180 degree sys
+	transformed = transform360To180(transformed)
+	
+	return transformed
+	
+	
+def transformAxisAngleTo360(axisAngle, axis = 1):
+	""" expresses orientation info from getAxisAngle in a 360 degrees system (2D!!!) """
+	angle360 = (360 + axisAngle[axis] * axisAngle[3]) % 360
+	return angle360
+	
+	
+def transform360To180(angle):
+	""" expresses angles in 360 degree system as +/- 180 degree angles (clockwise beeing positiv) """
+	angle180 = angle - 360 if angle > 180 else angle
+	return angle180
+	
+	
+def transformAxisAngleTo180(axisAngle, axis = 1):
+	""" transforms axisAngle data to a proper +/- 180 representation """
+	angle360 = transformAxisAngleTo360(axisAngle, axis)
+	angle180 = transform360To180(angle360)
+	return angle180
+
+
+def saveHomingVectors(avatar, output):
+	""" 
+	calculates the correct homing vectors for egocentric and allocentric reference frames
+	and saves them to the output dict
+	"""
+	# create helper object at the same position as the avatar
+	ignisFatuus = vizshape.addCube(size=1.0)
+	ignisFatuus.visible(viz.OFF)
+	yield ignisFatuus.setPosition(avatar.getPosition())
+	# let it look to the starting point
+	yield ignisFatuus.lookAt([0,0,0])
+	# get heading (avatarAxis) and allocentric homing in weird vizard coordinates
+	ignisAxis 	= ignisFatuus.getAxisAngle(viz.ABS_GLOBAL)
+	avatarAxis  = avatar.getAxisAngle(viz.ABS_GLOBAL)
+	
+	# convert to 360 system
+	heading360 = transformAxisAngleTo360(avatarAxis)
+	allo360    = transformAxisAngleTo360(ignisAxis)
+	
+	# get homing vectors in proper coordinates & save to output
+	output['alloHomingVector'] = transformAxisAngleTo180(ignisAxis, axis = 1)
+	output['egoHomingVector']  = transformReferenceFrame(heading360, allo360)
+
+
+
+
+
+##########################################################################
+##                              classes                                 ##
+##########################################################################
+
+
+class Starfield(viz.ActionClass):
+	"""
+	action class providing a starfield that can be used as optical flow stimulus
+	"""
+	
+	def begin(self,object):
+		"""
+		constructor: places the stars randomly columns around a column
+		center (black cube). columns are stored in a list
+		
+		"""
+		self.data 	   = self._actiondata_.data
+		self.nStars    = self.data[0]
+		self.starSize  = self.data[1]
+		self.colDimXYZ = self.data[2]
+		self.nColsXYZ  = self.data[3]
+		
+		# internal parameters
+		self.starsPerColumn = int(self.nStars / (self.nColsXYZ[0] * 
+							  self.nColsXYZ[1] * self.nColsXYZ[2]))
+		self.xDeltaMax = self.colDimXYZ[0] * self.nColsXYZ[0] / 2
+		self.yDeltaMax = self.colDimXYZ[1] * self.nColsXYZ[1] / 2
+		self.zDeltaMax = self.colDimXYZ[2] * self.nColsXYZ[2] / 2
+		self.countStart = 0
+		self.time = 0
+
+		# create a master star
+		protoStar = viz.addTexQuad(size = self.starSize)
+		protoStar.setPosition([0,1,1])
+		diff = viz.add('star.png')
+		#norm = viz.add('normal1.tif') 
+		protoStar.texture(diff,'',1)
+		#protoStar.bumpmap(norm,'',0) does not work with the fog
+		protoStar.billboard(mode=viz.BILLBOARD_VIEW)
+		
+		# list with all stars
+		self.columns = []
+		for idx_x in range(self.nColsXYZ[0]):
+			for idx_y in range(self.nColsXYZ[1]):
+				for idx_z in range(self.nColsXYZ[2]):
+					# create center node as parent for all stars in that column
+					columnCenter = vizshape.addCube(size = 0.001)
+					columnCenter.color(viz.BLACK)
+					columnCenter.setPosition([idx_x * self.colDimXYZ[0],
+											  idx_y * self.colDimXYZ[1],
+											  idx_z * self.colDimXYZ[2]])
+					self.columns.append(columnCenter)
+					# randomly create stars within the limits of one column
+					for y in range(self.starsPerColumn):
+						# positioning
+						x = self.colDimXYZ[0] * (random.random() - 0.5)
+						y = self.colDimXYZ[1] * (random.random() - 0.5)
+						z = self.colDimXYZ[2] * (random.random() - 0.5)
+						star = protoStar.clone(columnCenter)
+						star.setPosition([x,y,z], mode = viz.ABS_PARENT)
+						star.billboard(mode=viz.BILLBOARD_VIEW)
+		
+		protoStar.remove()
+
+
+
+	def update(self,elapsed,object):
+		"""
+		checks every frame if a column center exceeded the volume
+		if yes, it reenters the volume on the oppsite side (like in snake)
+		"""
+
+		# get position of the avatar (center of the field)
+		[aX, aY, aZ] = object.getPosition(mode = viz.ABS_GLOBAL)
+		# for every star: check if within limits of field, if not, reposition using respawn function
+		for column in self.columns[self.countStart::4]:
+			# get position of the star
+			pos = column.getPosition(mode = viz.ABS_GLOBAL)
+			# check every coordinate
+			x = pos[0] if abs(pos[0] - aX) <= self.xDeltaMax else self.respawn(aX, pos[0], self.xDeltaMax)
+			y = pos[1] if abs(pos[1] - aY) <= self.yDeltaMax else self.respawn(aY, pos[1], self.yDeltaMax)
+			z = pos[2] if abs(pos[2] - aZ) <= self.zDeltaMax else self.respawn(aZ, pos[2], self.zDeltaMax)
+
+			self.time = elapsed + self.time
+			
+			# eventually reposition
+			column.setPosition([x,y,z], mode = viz.ABS_GLOBAL)
+			
+		self.countStart = (self.countStart + 1) % 4
+		
+		if self.time >= 200.00 and self.time <= 200.1:
+			object.setPosition([0,0,0], viz.ABS_GLOBAL)
+			
+		
+	def end(self,object):
+		for column in self.columns:
+			column.remove()
+		print 'starfield ended'		
+		
+		
+	def respawn(self, cAvatar, cPoint, deltaMax):
+		""" if point exceeded displayed field, this calculates the reentry coordinates """
+		# transform into coordinate system with avatar as (0,0)
+		delta  = cPoint - cAvatar
+		# how far the point exceeded the limit
+		rest   = abs(delta) % deltaMax
+		# check on which side
+		side   = -1 if cPoint < cAvatar else 1
+		# calculate new coordinates in global sys after entering on the other side
+		new    = cAvatar - side * (deltaMax - rest)
+		return new
+
+
+def addStarfield(nStars = 10000, starSize = 0.18, colDimXYZ = [5, 30, 5], nColsXYZ = [20, 1, 20], fog = True):
+	"""
+	creates a starfield around the object it is assigned to as action
+	object is center and the cuboid has the edge lenghts dimX, dimY and dimZ
+	every frame every star is checked if it exceeds the cuboid, if yes it reenters
+	the cuboid on the other side (like playing good old snkae)
+	"""	
+	
+	if fog:
+		fogEnd = max([a*b for a,b in zip(colDimXYZ,nColsXYZ)]) / 2
+		fogStart = fogEnd - 2 * min(colDimXYZ)
+		viz.fog(fogStart,fogEnd)				
+		viz.fogcolor(viz.BLACK)
+		
+	bla 			= viz.ActionData()
+	bla.data 		= [nStars, starSize, colDimXYZ, nColsXYZ]
+	bla.actionclass	= Starfield
+	return bla
